f4b52de58ee085bd6370b7354913579e957f2242
Norman Herrera <norman.herrera@ymail.com>
2022-11-16 15:41:03 -0800

    fixed reassining value bug, assigns class

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -3,11 +3,13 @@ import './RuleRow.css'
 
 class RuleRow extends Component {
   render() {
+    const {score, name, doScore} = this.props;
+    const disabled = score != undefined;
     return (
-      <tr className="RuleRow RuleRow-active" 
-        onClick={this.props.score === undefined ? this.props.doScore : null}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+      <tr className={`RuleRow RuleRow-${score === undefined ? "active" : "disabled"}`} 
+        onClick={disabled ? null : doScore}>
+        <td className="RuleRow-name">{name}</td>
+        <td className="RuleRow-score">{score}</td>
       </tr>
     )
   }
